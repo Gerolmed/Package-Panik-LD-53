be24e9cdf6d8c10563ce05d4ed6279e2b355f4da
Gerolmed <werwolf420@yahoo.de>
2023-04-29 16:19:31 +0200

    Added tilemap graph reader

---
+++ Assets/Scripts/ludumdare/WorldGraph/TilemapGraph/TilemapReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+namespace LudumDare.WorldGraph.TilemapGraph
+{
+    public class TilemapReader: MonoBehaviour
+    {
+        [SerializeField]
+        private Tilemap tilemap;
+
+        [SerializeField]
+        private GraphEvent onGenerate;
+
+        private void Start()
+        {
+            var graph = new Graph<NodeData>();
+            var bounds = tilemap.cellBounds;
+
+            for (var x = bounds.x; x < bounds.xMax; x++)
+            {
+                for (var y = bounds.y; y < bounds.yMax; y++)
+                {
+                    var tileBase = tilemap.GetTile(new Vector3Int(x, y, 0));
+                    if(tileBase is not NodeTile nodeTile || nodeTile.DirectionMask  == DirectionMask.None) continue;
+                    graph.AddNodeAt(new Vector2Int(x, y), new NodeData(),
+                        nodeTile.DirectionMask);
+                }
+            }
+            
+            onGenerate.Invoke(graph);
+        }
+    }
+    
+    [Serializable]
+    public class GraphEvent: UnityEvent<Graph<NodeData>> {}
+}

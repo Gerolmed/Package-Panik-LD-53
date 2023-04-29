0c497fdead729e91254b68d0fd62904b107c3018
Gerolmed <werwolf420@yahoo.de>
2023-04-29 18:48:16 +0200

    Added district management

---
+++ Assets/Scripts/ludumdare/Districts/DistrictManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LudumDare.Districts
+{
+    public class DistrictManager: MonoBehaviour, IDistrictManager
+    {
+        [SerializeField]
+        private DistrictManagerSocket districtManagerSocket;
+        [SerializeField]
+        private DistrictEvent onUnlock;
+
+        private List<District> _districts = new();
+        private readonly HashSet<string> _unlocked = new();
+
+        private void Awake()
+        {
+            districtManagerSocket.Instance = this;
+        }
+
+
+        public IEnumerable<District> GetAll()
+        {
+            return _districts;
+        }
+
+
+        public IEnumerable<District> GetUnlocked()
+        {
+            return GetAll().Where(district => !IsLocked(district));
+        }
+
+
+        public IEnumerable<District> GetLocked()
+        {
+            return GetAll().Where(IsLocked);
+        }
+
+
+        public void Unlock(District district)
+        {
+            _unlocked.Add(district.ID);
+            onUnlock.Invoke(district);
+        }
+
+
+        public bool IsLocked(District district)
+        {
+            return _unlocked.Contains(district.ID);
+        }
+
+
+        public void Setup(List<District> districts)
+        {
+            _unlocked.Clear();
+            _districts = districts;
+        }
+
+
+        public District GetAt(Vector2Int node)
+        {
+            return _districts.FirstOrDefault(district => district.Contains(node));
+        }
+    }
+    
+    [Serializable]
+    public class DistrictEvent: UnityEvent<District> {}
+}
---
+++ Assets/Scripts/ludumdare/Districts/DistrictManagerSocket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LudumDare.Districts
+{
+    [CreateAssetMenu(menuName = "LD/District Manager Socket", order = 0)]
+    public class DistrictManagerSocket : ScriptableObject
+    {
+        public IDistrictManager Instance { get; set; }
+
+
+        private void Awake()
+        {
+            Instance = null;
+        }
+
+
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+    }
+}

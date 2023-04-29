b76c66e44f531293744cba4d311ee24f73590796
Gerolmed <werwolf420@yahoo.de>
2023-04-29 12:35:59 +0200

    Added money manager

---
+++ Assets/Scripts/ludumdare/MoneySystem/MoneyManagerSocket.cs
@@ -0,0 +1,23 @@
+﻿using LudumDare.TimeControl;
+using UnityEngine;
+
+namespace LudumDare.MoneySystem
+{
+    [CreateAssetMenu(menuName = "LD/Money Manager Socket", order = 0)]
+    public class MoneyManagerSocket : ScriptableObject
+    {
+        public IMoneyManager Instance { get; set; }
+
+
+        private void Awake()
+        {
+            Instance = null;
+        }
+
+
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+    }
+}
---
+++ Assets/Scripts/ludumdare/Utils/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LudumDare.Utils
+{
+    public static class Extensions
+    {
+        public static V GetOrCreate<K, V>(this Dictionary<K, V> dict, K key, Func<V> onMissing)
+        {
+            if (dict.TryGetValue(key, out var outVal)) return outVal;
+            
+            outVal = onMissing.Invoke();
+            dict.Add(key, outVal);
+
+            return outVal;
+        }
+        public static V ComputeOrCreate<K, V>(this Dictionary<K, V> dict, K key, Func<V> onMissing, Func<V, V> doCompute)
+        {
+            if (dict.TryGetValue(key, out var outVal))
+            {
+                return dict[key] = doCompute.Invoke(outVal);
+            }
+            
+            outVal = onMissing.Invoke();
+            dict.Add(key, outVal);
+            return dict[key] = doCompute.Invoke(outVal);
+        }
+    }
+}

b95b4914c80cf63e0d0f3fb94e68fec3fca4244b
Gerolmed <werwolf420@yahoo.de>
2023-04-29 11:19:22 +0200

    Added unit

---
+++ Assets/Scripts/ludumdare/Units/DeliveryUnit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace LudumDare.Units
+{
+    [CreateAssetMenu(menuName = "Units", order = 0)]
+    public class DeliveryUnit : ScriptableObject
+    {
+        
+    }
+}

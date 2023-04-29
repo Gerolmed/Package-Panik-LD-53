2159415e8ee37bc983ddf1205b7c1aa22a32cfc3
andrground <av.andreevv@gmail.com>
2023-04-29 22:34:07 +0200

    Added support for animated buildings

--- Assets/Scripts/ludumdare/Hub/Buildings/BuildingLevel.cs
+++ Assets/Scripts/ludumdare/Hub/Buildings/BuildingLevel.cs
@@ -8,8 +8,10 @@ namespace LudumDare.Hub.Buildings
         [SerializeField] private Sprite sprite;
         [SerializeField] private string description;
         [SerializeField] private int price;
+        [SerializeField] private RuntimeAnimatorController animatorController = null;
         public Sprite Sprite => sprite;
         public string Description => description;
         public int Price => price;
+        public RuntimeAnimatorController AnimatorController => animatorController;
     }
 }
--- Assets/Scripts/ludumdare/Hub/Buildings/BuildingRenderer.cs
+++ Assets/Scripts/ludumdare/Hub/Buildings/BuildingRenderer.cs
@@ -6,9 +6,13 @@ namespace LudumDare.Hub.Buildings
     public class BuildingRenderer : MonoBehaviour
     {
         [SerializeField] private Image image;
+        [SerializeField] private Animator animator = null;
         public void Render((int level, BuildingLevel buildingLevel, BuildingLevel nextLevel) upgrade)
         {
             image.sprite = upgrade.buildingLevel.Sprite;
+            
+            if (animator) 
+                animator.runtimeAnimatorController = upgrade.buildingLevel.AnimatorController;
         }   
     }
 }

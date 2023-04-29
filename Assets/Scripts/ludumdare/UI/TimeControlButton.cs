8ae6880176ed8b13c4f5e14ed63aae39b1408bf2
Gerolmed <werwolf420@yahoo.de>
2023-04-30 01:16:40 +0200

    Added time controls to UI

---
+++ Assets/Scripts/ludumdare/UI/TimeControlButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LudumDare.TimeControl;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace LudumDare.UI
+{
+    public class TimeControlButton: UIBehaviour
+    {
+        [SerializeField]
+        private TimeControlButtonEvent onClick;
+
+        [SerializeField]
+        private TimeMode timeMode;
+        
+        [SerializeField]
+        private Sprite normalState;
+        
+        [SerializeField]
+        private Sprite pressedState;
+
+        public TimeMode TimeMode => timeMode;
+
+
+        public void DoClick()
+        {
+            onClick.Invoke(this);
+        }
+
+
+        public void SetActive(bool active)
+        {
+            GetComponent<Image>().sprite = active ? pressedState : normalState;
+        }
+    }
+    
+    [Serializable]
+    public class TimeControlButtonEvent: UnityEvent<TimeControlButton> {}
+}
---
+++ Assets/Scripts/ludumdare/UI/TimeControls.cs
@@ -0,0 +1,23 @@
+﻿using LudumDare.TimeControl;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LudumDare.UI
+{
+    public class TimeControls: UIBehaviour
+    {
+        [SerializeField]
+        private TimeControlManagerSocket controlManagerSocket;
+        
+        public void OnClick(TimeControlButton button)
+        {
+
+            controlManagerSocket.Instance.TimeMode = button.TimeMode;
+            var buttons = GetComponentsInChildren<TimeControlButton>();
+            foreach (var timeControlButton in buttons)
+            {
+                timeControlButton.SetActive(timeControlButton == button);
+            }
+        }
+    }
+}

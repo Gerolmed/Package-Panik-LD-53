80638f46419d206fd2d1d10671299bff93be5b30
Gerolmed <werwolf420@yahoo.de>
2023-04-29 12:07:37 +0200

    Added time manager functionality

--- Assets/Scripts/ludumdare/TimeControl/ITimeControlManager.cs
+++ Assets/Scripts/ludumdare/TimeControl/ITimeControlManager.cs
@@ -2,6 +2,15 @@
 {
     public interface ITimeControlManager
     {
-        
+        public float TimeModifier { get; }
+        public TimeMode TimeMode { set; get; }
+        public bool IsPaused => TimeMode == TimeMode.Paused;
+    }
+
+    public enum TimeMode
+    {
+        Paused,
+        Normal,
+        Fast
     }
 }
--- Assets/Scripts/ludumdare/TimeControl/TimeControlManager.cs
+++ Assets/Scripts/ludumdare/TimeControl/TimeControlManager.cs
@@ -6,11 +6,27 @@ namespace LudumDare.TimeControl
     {
         [SerializeField]
         private TimeControlManagerSocket socketRef;
-
-
+        
         private void Awake()
         {
             socketRef.Instance = this;
+
+            TimeMode = TimeMode.Normal;
+        }
+        
+
+        public float TimeModifier { private set; get; }
+
+        private TimeMode _timeMode;
+
+        public TimeMode TimeMode
+        {
+            get => _timeMode;
+            set
+            {
+                _timeMode = value;
+                TimeModifier = (float) _timeMode;
+            }
         }
     }
 }

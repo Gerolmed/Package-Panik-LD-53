ea65b8a3427f2c5f3e190c6b3882411c854f9197
Gerolmed <werwolf420@yahoo.de>
2023-04-29 11:31:26 +0200

    Added unity weight param

--- Assets/Scripts/ludumdare/Units/DeliveryUnit.cs
+++ Assets/Scripts/ludumdare/Units/DeliveryUnit.cs
@@ -2,9 +2,40 @@
 
 namespace LudumDare.Units
 {
-    [CreateAssetMenu(menuName = "Units", order = 0)]
+    [CreateAssetMenu(menuName = "Unit", order = 0)]
     public class DeliveryUnit : ScriptableObject
     {
+        [SerializeField]
+        private DeliveryType deliveryType;
+
+        public DeliveryType DeliveryType => deliveryType;
         
+        [SerializeField]
+        private NavigationType navType;
+
+        public NavigationType NavType => navType;
+        
+        
+        [SerializeField]
+        private int totalWeight = 1;
+        public int TotalWeight => totalWeight;
+        
+        [SerializeField]
+        private int maintenanceCost = 1;
+        public int MaintenanceCost => maintenanceCost;
+        [SerializeField]
+        private int purchaseCost = 1;
+        public int PurchaseCost => purchaseCost;
+    }
+    
+    public enum NavigationType
+    {
+            Grid,
+            Air
+    }
+    public enum DeliveryType
+    {
+            Mail,
+            Package
     }
 }

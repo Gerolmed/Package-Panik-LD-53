da8d43e4ca046fdc4c3ea4baf97408dc540be02b
Gerolmed <werwolf420@yahoo.de>
2023-04-30 23:39:38 +0200

    Added main menu

---
+++ Assets/Scripts/ludumdare/UI/MainMenuControls.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LudumDare.UI
+{
+    public class MainMenuControls: MonoBehaviour
+    {
+        public void EnterGame()
+        {
+            SceneManager.LoadScene("Scenes/MainWorld");
+        }
+    }
+}

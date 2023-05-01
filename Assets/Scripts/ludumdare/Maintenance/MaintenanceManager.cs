72309aa94513c1ae606817114801b1fcecd20479
andrground <av.andreevv@gmail.com>
2023-05-02 01:30:28 +0200

    Implemented maintenance + first balance attempt

---
+++ Assets/Scripts/ludumdare/Maintenance/MaintenanceManager.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using UnityEngine;
+using LudumDare.MoneySystem;
+using LudumDare.TimeControl;
+using LudumDare.Units;
+
+namespace LudumDare.Maintenance
+{
+    public class MaintenanceManager : MonoBehaviour, IMaintenanceManager
+    {
+        [SerializeField]
+        private MaintenanceManagerSocket maintenanceManagerSocket;
+
+        [SerializeField]
+        private TimeControlManagerSocket timeControlManagerSocket;
+
+        [SerializeField]
+        private MoneyManagerSocket moneyManagerSocket;
+
+        [SerializeField]
+        private DeliveryUnitStorageSocket deliveryUnitStorageSocket;
+        private void Awake()
+        {
+            maintenanceManagerSocket.Instance = this;
+        }
+
+        public void CheckCycle() {
+            if (timeControlManagerSocket.Instance.Cycle % timeControlManagerSocket.Instance.CyclesPerDay == 0)
+            {
+                DeductMoney();
+            }
+        }
+
+        private void DeductMoney()
+        {
+            deliveryUnitStorageSocket.Instance.GetUnits()
+                .ToList()
+                .ForEach(unit => moneyManagerSocket.Instance.TryDeduct(unit.Type.MaintenanceCost, TransactionType.Maintenance));
+        }
+    }
+}

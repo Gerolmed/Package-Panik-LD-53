37d90c1cab4c40676bf342cea12bddb5ba4710aa
CreepyPvP <interruptedThread@gmail.com>
2023-05-01 18:12:21 +0200

    Delivery generator uses noise function now

--- Assets/Scripts/ludumdare/Delivery/DeliveryGenerator.cs
+++ Assets/Scripts/ludumdare/Delivery/DeliveryGenerator.cs
@@ -23,6 +23,8 @@ namespace LudumDare.Delivery
         private HashSet<Vector2Int> _targets = new();
         private Dictionary<string, List<Vector2Int>> _targetCache = new();
 
+        private RandomMathFunction _packetDistributionFunction = new RandomMathFunction();
+
         public void OnGraph(Graph<NodeData> graph)
         {
             _targets = graph.NodeGraph
@@ -39,7 +41,9 @@ namespace LudumDare.Delivery
                 var possibleTargets = _targetCache.GetOrCreate(district.ID,
                     () => CollectAllFor(district));
 
-                var targets = DistributeOnto(possibleTargets, 5);
+                var age = cycle - (int) district.UnlockedSince;
+
+                var targets = DistributeOnto(possibleTargets, (int) _packetDistributionFunction.GetRandomValue(age));
                 resolver.ExecuteDelivery(targets.Select(target => new DeliveryCommand(target, DeliveryType.Mail)));
 
             }
